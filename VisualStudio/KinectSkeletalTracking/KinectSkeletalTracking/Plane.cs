755ab97bdf8f610f0466cb6aed5ce3a319b41613
Thomas Bartleet <42772107+ThomasBartleet@users.noreply.github.com>
2021-03-11 15:11:27 +0000

    Created a Plane class

---
+++ VisualStudio/KinectSkeletalTracking/KinectSkeletalTracking/Plane.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectSkeletalTracking
+{
+    public class Plane : IEquatable<Plane>
+    {
+        public double X { get; set; } = 0.0;
+        public double Y { get; set; } = 0.0;
+        public double Z { get; set; } = 0.0;
+        public double D { get; set; } = 0.0;
+
+        public Plane(double x = 0, double y = 0, double z = 0, double d = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            D = d;
+        }
+
+        #region Operations
+
+        public static bool operator ==(Plane thisPlane, Plane otherPlane)
+        {
+            return thisPlane.X == otherPlane.X && thisPlane.Y == otherPlane.Y && thisPlane.Z == otherPlane.Z;
+        }
+
+        public static bool operator !=(Plane thisPlane, Plane otherVector)
+        {
+            return !(thisPlane == otherVector);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is Plane && Equals(obj as Plane);
+        }
+
+        public bool Equals(Plane other)
+        {
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                hash = hash * 23 + Z.GetHashCode();
+                hash = hash * 23 + D.GetHashCode();
+                return hash;
+            }
+        }
+
+        #endregion
+
+        public Vector3 AsVector()
+        {
+            return new Vector3(X, Y, Z);
+        }
+
+        public static Plane FromPoints(Point3 a, Point3 b, Point3 c)
+        {
+            Vector3 AB = Vector3.FromPoints(a, b);
+            Vector3 AC = Vector3.FromPoints(a, c);
+
+            // Do the cross product
+            Vector3 normal = AB.Cross(AC);
+
+            // Return the normal with the ammended D value. This is the Plane equation.
+            return new Plane
+            {
+                X = normal.X,
+                Y = normal.Y,
+                Z = normal.Z,
+                D = -((normal.X * (-a.X)) + (normal.Y * (-b.Y)) + (normal.Z * (-c.Z)))
+            };
+        }
+
+
+        public static Plane FromVectors(Vector3 u, Vector3 v)
+        {
+            // Do the cross product
+            Vector3 orthogonal = u.Cross(v);
+
+            // Return the normal with the ammended D value. This is the Plane equation.
+            return new Plane
+            {
+                X = orthogonal.X,
+                Y = orthogonal.Y,
+                Z = orthogonal.Z,
+                D = -((orthogonal.X * (-u.X)) + (orthogonal.Y * (-u.Y)) + (orthogonal.Z * (-u.Z)))
+            };
+        }
+
+        public static double GetAngleBetweenPlanes(Plane plane1, Plane plane2, bool inRadians = false)
+        {
+            Vector3 vector1 = plane1.AsVector();
+            Vector3 vector2 = plane2.AsVector();
+
+            return Vector3.GetAngleBetweenVectors(vector1, vector2, inRadians);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Plane[X:{0}, Y:{1}, Z:{2}, D:{3}]", X, Y, Z, D);
+        }
+    }
+}
---
+++ VisualStudio/KinectSkeletalTracking/UnitTests/Plane.cs
@@ -0,0 +1,73 @@
+﻿using KinectSkeletalTracking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PlaneTests
+    {
+        [TestClass]
+        public class Constructors
+        {
+            [TestMethod]
+            public void Constructor()
+            {
+                Plane plane = new Plane(1, 2, 3, 4);
+
+                Assert.AreEqual(1, plane.X);
+                Assert.AreEqual(2, plane.Y);
+                Assert.AreEqual(3, plane.Z);
+                Assert.AreEqual(4, plane.D);
+            }
+
+            [TestMethod]
+            public void FromPoints()
+            {
+                Plane plane = Plane.FromPoints(
+                    new Point3(1, 0, 0),
+                    new Point3(0, 1, 0),
+                    new Point3(0, 0, 1)
+                );
+
+                Plane expectedPlane = new Plane(1, 1, 1, -1);
+
+                Assert.AreEqual(expectedPlane, plane);
+            }
+
+            [TestMethod]
+            public void FromVectors()
+            {
+                Plane plane = Plane.FromVectors(
+                    new Vector3(1, 0, 0),
+                    new Vector3(0, 1, 0)
+                );
+
+                Plane expectedPlane = new Plane(0, 0, 1, 0);
+
+                Assert.AreEqual(expectedPlane, plane);
+            }
+        }
+
+        [TestMethod]
+        public void AsVector()
+        {
+            Plane plane = new Plane(1, 1, 1, -1);
+            Vector3 vector = plane.AsVector();
+
+            Vector3 expectedVector = new Vector3(1, 1, 1);
+
+            Assert.AreEqual(expectedVector, vector);
+        }
+
+        [TestMethod]
+        public void GetAngleBetweenPlanes()
+        {
+            Plane plane1 = new Plane(0, 0, 1, 0);
+            Plane plane2 = new Plane(1, 0, 0, 0);
+
+            double angle = Plane.GetAngleBetweenPlanes(plane1, plane2);
+
+            Assert.AreEqual(90, angle);
+        }
+    }
+}
