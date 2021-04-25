b94ba97f75975a58dcedb96b88f51ff541e6e61e
Thomas Bartleet <42772107+ThomasBartleet@users.noreply.github.com>
2021-04-25 11:05:13 +0100

    Added custom functions to set the range of motors
    The library functions don't work as well. They come up with errors. So, I created custom functions that actually do hat they're supposed to do, according to the Dynamixel Protocol 1.0.

--- Arduino/RobotArm/DynamixelMotorControl.cpp
+++ Arduino/RobotArm/DynamixelMotorControl.cpp
@@ -5,6 +5,8 @@
 #include <stdint.h>
 
 uint16_t ConvertToServoPosition(const int16_t angle, const bool reverse = false);
+int set_min_range(uint8_t ID, uint16_t min_range);
+int set_max_range(uint8_t ID, uint16_t max_range);
 
 void MoveToAngle(uint8_t const id, const int16_t inputAngle, const bool reverse)
 {
@@ -37,6 +39,8 @@ void DynamixelInit()
 //    Dynamixel.setMaxTorque(254,512);        // 50% of Torque
 //    Dynamixel.setSRL(254,2);                // Set the SRL to Return All
 
+    set_min_range(BROADCAST_ID, SERVO_MIN_POSITION);
+    set_max_range(BROADCAST_ID, SERVO_MAX_POSITION);
 //    Dynamixel.setAngleLimit(BROADCAST_ID, SERVO_MIN_POSITION, SERVO_MAX_POSITION);
 //    Dynamixel.setCMargin(BROADCAST_ID, 0, 0);
 //    Dynamixel.setCSlope(BROADCAST_ID, 50, 50);
@@ -58,4 +62,54 @@ uint16_t ConvertToServoPosition(const int16_t angle, const bool reverse)
     position += servoAngle;
 
     return position;
+}
+
+int set_min_range(uint8_t ID, uint16_t min_range) {
+  const uint8_t len = 5;
+  const uint8_t reg = 6;
+  uint8_t range_upper = (min_range >> 8) & 0xFF;
+  uint8_t range_lower = min_range & 0xFF;
+  uint8_t Checksum = (~(ID + len + AX_WRITE_DATA + reg + range_upper + range_lower)) & 0xFF;
+  
+  digitalWrite(DIRECTION_PIN, HIGH);
+  
+  Serial2.write(AX_START);                // Send Instructions over Serial
+  Serial2.write(AX_START);
+  Serial2.write(ID);
+  Serial2.write(len);
+  Serial2.write(AX_WRITE_DATA);
+  Serial2.write(reg);
+  Serial2.write(range_lower);
+  Serial2.write(range_upper);
+  Serial2.write(Checksum);
+  Serial2.flush();
+  
+  digitalWrite(DIRECTION_PIN, LOW);
+
+  return Dynamixel.read_error();
+}
+
+int set_max_range(uint8_t ID, uint16_t max_range) {
+  const uint8_t len = 5;
+  const uint8_t reg = 8;
+  uint8_t range_upper = (max_range >> 8) & 0xFF;
+  uint8_t range_lower = max_range & 0xFF;
+  uint8_t Checksum = (~(ID + len + AX_WRITE_DATA + reg + range_upper + range_lower)) & 0xFF;
+  
+  digitalWrite(DIRECTION_PIN, HIGH);
+  
+  Serial2.write(AX_START);                // Send Instructions over Serial
+  Serial2.write(AX_START);
+  Serial2.write(ID);
+  Serial2.write(len);
+  Serial2.write(AX_WRITE_DATA);
+  Serial2.write(reg);
+  Serial2.write(range_lower);
+  Serial2.write(range_upper);
+  Serial2.write(Checksum);
+  Serial2.flush();
+  
+  digitalWrite(DIRECTION_PIN, LOW);
+
+  return Dynamixel.read_error();
 }
